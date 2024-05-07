a7afcdd358021f2b33ef5958e15beaa42ec40b64
marwaZbidi <zbidimarwa0@gmail.com>
2024-05-07 19:05:21 +0100

    modification reservation

--- camp/back/controllers/reservation/cancelReservation.js
+++ camp/back/controllers/reservation/cancelReservation.js
@@ -2,12 +2,13 @@ const reservations = require ('../../models/Client/reservation')
 
 const cancelReservation = async (req, res) => {
     try {
-        const {reservationId} = req.params
-        const status = req.body.reservation
-        await reservations.update({
-            where: {reservationId}
+        const reservationId = req.params
+        const cancel = req.body.status
+        await reservations.update(
+           {status: cancel},
+            {where: {reservationId}
         })
-        res.status(200).json(status)
+        res.status(200).json(cancel)
     } catch (error) {
         console.log(error)
         res.status(500).json({message: 'Internal server error'})
